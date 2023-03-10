89a986aa7dd38158efcd27e904340e2a1f097c17
Zan Owsley <zanowsley@gmail.com>
2023-03-10 16:21:41 -0700

    initial commit

---
+++ ex11.py
@@ -0,0 +1,16 @@
+print('How old are you?', end=' ')
+age = input()
+print('How tall are you?', end=' ')
+height = input()
+print('How much do you weigh?', end=' ')
+weight = input()
+
+print(f"So you're {age} years old, {height} tall, and {weight} pounds of pure chub.")
+
+# ex12
+
+age = input('How old are you? ')
+height = input('How tall are you? ')
+weight = input('How freaking heavy are you? ')
+
+print("So you're a big fella...")
---
+++ ex13.py
@@ -0,0 +1,5 @@
+from sys import argv
+
+a, b, c, d = argv
+
+print('the first is {}, the second is {}, the third is {}, and the fourth is {}'.format(a, b, c, d))
